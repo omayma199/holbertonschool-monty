ef643c279e99a63ed525acc4975e07d20162d1b9
omayma199 <5752@holbertonstudents.com>
2022-12-20 19:46:42 +0100

    callfunction

--- op_call.c
+++ op_call.c
@@ -47,3 +47,36 @@ void call(char **tokens, stack_t **stack)
 		exit(EXIT_FAILURE);
 	}
 }
+/**
+ * is_valid - check if second spot of a token array is valid
+ * @token: tokens value to check
+ * @stack: pointer to a stack
+ */
+void is_valid(char **token, stack_t **stack)
+{
+	int idx = 0;
+
+	if (!token[1])
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		if (*stack)
+			free_stack(stack);
+		free(token);
+		exit(EXIT_FAILURE);
+	}
+
+	while (token[1][idx])
+	{
+		if (token[1][idx] == '-' && idx == 0)
+			idx++;
+		if (isdigit(token[1][idx]) == 0)
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			if (*stack)
+				free_stack(stack);
+			free(token);
+			exit(EXIT_FAILURE);
+		}
+		idx++;
+	}
+}
